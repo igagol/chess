0ace08fc4849e799afb9c5426b0398b59ae003de
tomasz-stelagowski <tomasz.stelagowski@gmail.com>
2019-05-11 04:03:20 +0200

    Added logging and some comments

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log("DOM fully loaded and parsed");
 
+    console.log("Drawing chessboard");
+
     drawChessBoard();
+
+    console.log("setting up pieces");
+
     setWolfAndSheeps();
     
+    console.log("Setting up click events");
+
     setOnFigureClickEvents();
 });
